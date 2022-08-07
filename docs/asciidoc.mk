dc5fdc8710cd201733d0f70f489d7c4bdda99d87
unmyr <56982541+unmyr@users.noreply.github.com>
2022-08-07 23:10:34 +0900

    docs: Change the theme to "golo.css".

--- docs/asciidoc.mk
+++ docs/asciidoc.mk
@@ -2,8 +2,12 @@ IMAGES_DIR=images
 CP_OPTS=-n -l
 ROUGE_STYLE=monokai_sublime
 
+STYLESHEET_FILE=golo.css
+STYLESHEET_DIR=$(BASE_DIR)/docs/stylesheets
+
 AD_OPTS_HTML5_COMMON=\
 --backend=html5 \
+-a stylesheet=$(STYLESHEET_FILE) -a stylesdir=$(STYLESHEET_DIR) \
 -a imagesdir=$(IMAGES_DIR)
 
 AD_OPTS_HTML5_ROUGE=\
